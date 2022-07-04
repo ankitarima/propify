3a9a7698d87d0860b83c9fd945d40bfee2fa1143
ankitarima <ankitsha77@gmail.com>
2022-07-04 22:02:13 +0530

    seetings

--- client/src/app/modules/features/settings/channel-partners/channel-partners.component.ts
+++ client/src/app/modules/features/settings/channel-partners/channel-partners.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChannelPartnersComponent implements OnInit {
 
+  public display:boolean = false;
+  public showBrokers:boolean = true;
+  public displayUserform:boolean = false;
+  public displayDelete:boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addUser(){
+    this.displayUserform = true;
+  }
+  editUser(){
+    this.displayUserform = true;
+  }
+
+  deleteUser(){
+    this.displayDelete = true;
   }
 
 }
--- client/src/app/modules/features/settings/users/users.component.ts
+++ client/src/app/modules/features/settings/users/users.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  public displayUserform:boolean = false;
+  public displayDelete:boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addUser(){
+    this.displayUserform = true;
+  }
+  editUser(){
+    this.displayUserform = true;
+  }
+
+  deleteUser(){
+    this.displayDelete = true;
   }
 
 }
